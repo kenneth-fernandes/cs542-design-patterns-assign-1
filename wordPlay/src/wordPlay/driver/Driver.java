dc04cba5aa29f0ca52ef8ec641880d906d4ab067
kenneth-fernandes <kenferns9339@gmail.com>
2020-01-31 20:52:13 -0500

    Kenneth: Testing the commit from VSCode to repo after Build.

--- wordPlay/src/wordPlay/driver/Driver.java
+++ wordPlay/src/wordPlay/driver/Driver.java
@@ -8,15 +8,19 @@ public class Driver {
 	public static void main(String[] args) {
 
 		/*
-		 * As the build.xml specifies the arguments as argX, in case the
-		 * argument value is not given java takes the default value specified in
-		 * build.xml. To avoid that, below condition is used
+		 * As the build.xml specifies the arguments as argX, in case the argument value
+		 * is not given java takes the default value specified in build.xml. To avoid
+		 * that, below condition is used
 		 */
-		if ((args.length != 3) || (args[0].equals("${arg0}")) || (args[1].equals("${arg1}")) || (args[2].equals("${arg2}"))) {
+		if ((args.length != 3) || (args[0].equals("${arg0}")) || (args[1].equals("${arg1}"))
+				|| (args[2].equals("${arg2}"))) {
 			System.err.println("Error: Incorrect number of arguments. Program accepts 3 arguments.");
 			System.exit(0);
 		}
-		System.out.println("Hello World!");
+
+		for (int i = 0; i < args.length; i += 1) {
+			System.out.println("Args " + (i + 1) + " : " + args[i]);
+		}
 
 	}
 }
